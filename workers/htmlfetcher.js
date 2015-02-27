a4a1b38d4b5d317ad384c69e190bd6391d69bd1a
sungminchang <4sungmin@gmail.com>
2015-02-26 16:50:56 -0800

    Commit htmlfetcher and an archived google website.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,16 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+var fs = require('fs');
+var http = require('http');
+var request = require('request');
+
+
+exports.fetchWebsite = function(url){
+
+  request('http://' + url).pipe(fs.createWriteStream("../web/archives/sites/" + url));
+
+};
+
+//fetgc
+
+
